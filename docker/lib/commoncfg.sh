f6e2d6a6255d7b38c321f734ff2874bf393b934e
mooleshacat <43627985+mooleshacat@users.noreply.github.com>
2025-08-10 13:52:19 -0400

    add all files as they are currently

---
+++ docker/lib/commoncfg.sh
@@ -0,0 +1,14 @@
+
+#
+##
+## Import common configuration variables
+##
+## This is the new commonconfig.cfg mostly for user to edit but also can be appended automatically
+## 
+## WARNING THIS FILE MAY BE AUTOMATICALLY APPENDED TO
+##
+#
+
+# UNCOMMENT THIS FOR CUT DOWN OR CUSTOM INSTALLATION & CONFIGURE IT (without cloning git repository)
+#export GIT_ROOT=/root/sd-forge-webui-docker
+
